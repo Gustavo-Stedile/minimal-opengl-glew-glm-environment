3010e0a37336b199043368ae29017f1f6239a869
stledi <gustavostedile1@hotmail.com>
2025-06-19 12:33:08 -0300

    a sample code that displays a rotating cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,204 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
+#include <cmath>
+
+// Shaders
+const char *vertexShaderSource = R"glsl(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+out vec3 ourColor;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    ourColor = aColor;
+}
+)glsl";
+
+const char *fragmentShaderSource = R"glsl(
+#version 330 core
+in vec3 ourColor;
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(ourColor, 1.0);
+}
+)glsl";
+
+// Configurações da janela
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Ângulo de rotação
+float rotationAngle = 0.0f;
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    // Inicializa GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Falha ao inicializar GLFW" << std::endl;
+        return -1;
+    }
 
-    // TODO: seu código aqui:
+    // Configura GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Cria janela
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Cubo 3D com OpenGL", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Falha ao criar janela GLFW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Inicializa GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Falha ao inicializar GLEW" << std::endl;
+        return -1;
+    }
+
+    // Configura viewport
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glEnable(GL_DEPTH_TEST);
+
+    // Compila shaders
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Dados do cubo (vértices e cores)
+    float vertices[] = {
+        // Posições          // Cores
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+        -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f};
+
+    unsigned int indices[] = {
+        0, 1, 2, 2, 3, 0, // Face frontal
+        1, 5, 6, 6, 2, 1, // Face direita
+        5, 4, 7, 7, 6, 5, // Face traseira
+        4, 0, 3, 3, 7, 4, // Face esquerda
+        3, 2, 6, 6, 7, 3, // Face superior
+        4, 5, 1, 1, 0, 4  // Face inferior
+    };
+
+    // Configura buffers
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Atributos de posição
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // Atributos de cor
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Matrizes de transformação
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::lookAt(
+        glm::vec3(2.0f, 2.0f, 3.0f), // Posição da câmera
+        glm::vec3(0.0f, 0.0f, 0.0f), // Ponto de foco
+        glm::vec3(0.0f, 1.0f, 0.0f)  // Vetor "para cima"
+    );
+    glm::mat4 projection = glm::perspective(
+        glm::radians(45.0f),
+        (float)SCR_WIDTH / (float)SCR_HEIGHT,
+        0.1f, 100.0f);
+
+    // Loop de renderização
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        // Limpa buffers
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Ativa shader
+        glUseProgram(shaderProgram);
+
+        // Atualiza matriz de modelo (rotação)
+        rotationAngle += 0.5f;
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(rotationAngle), glm::vec3(0.5f, 1.0f, 0.0f));
+
+        // Passa matrizes para o shader
+        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        // Renderiza o cubo
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        // Troca buffers e verifica eventos
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Limpeza
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
     return 0;
 }
